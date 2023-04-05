e31c35c871badfba4914691815a8d5ad7c0b4119
Maksat <maksattologonovn@gmail.com>
2023-04-06 02:41:59 +0600

    initial

---
+++ src/menu/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+    url = models.CharField(max_length=100, blank=True)
+
+    def str(self):
+        return self.name
